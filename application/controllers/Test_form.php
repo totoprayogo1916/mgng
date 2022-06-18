a0ea389d20961111f7491ce617ace5ebcc29b410
Toto <mail@totoprayogo.com>
2022-06-18 19:13:01 +0700

    Perbaikan upload config

--- application/controllers/Test_form.php
+++ application/controllers/Test_form.php
@@ -9,11 +9,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Test_form extends CI_Controller
 {
-
-    function __construct()
+    public function __construct()
     {
         parent::__construct();
         // $this->load->model('usermodel');
+        $this->load->library('upload');
     }
 
     // public function index()
@@ -40,6 +40,22 @@ class Test_form extends CI_Controller
     // }
     public function index()
     {
+        // $data['title'] = "FREEWORK";
+        // $data['kategori'] = $this->model_produk->get_kategori_query();
+        // $data['subkategori'] = $this->model_produk->subkategori();
+        // $data['category'] = $this->model_produk->category();
+        // $data['subcategory'] = $this->model_produk->subcategory();
+        // $data['jenis'] = $this->model_produk->jenis();
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        // $this->load->view('templates/header', $data);
+        // $this->load->view('templates/topbar', $data);
+        $this->load->view('templates/js', $data);
+        $this->load->view('test_form', $data);
+        // $this->load->view('templates/footer');
+    }
+
+    public function up()
+    {
         if ($this->input->post('finish')) {
             $this->form_validation->set_rules('username', 'username', 'trim|required');
             $this->form_validation->set_rules('nama_depan', 'nama depan', 'trim|required');
@@ -53,16 +69,16 @@ class Test_form extends CI_Controller
             $this->form_validation->set_rules('jenis_kelamin', 'Jenis Kelamin', 'trim|required');
             $this->form_validation->set_rules('alamat', 'Alamat', 'trim|required');
             $this->form_validation->set_rules('kode_pos', 'Kode Pos', 'trim|required');
-            if ($this->form_validation->run() !== FALSE) {
 
+            if ($this->form_validation->run() !== false) {
                 $upload_image = $_FILES['image']['name'];
 
                 if ($upload_image) {
-                    $config['upload_path']          = './assets/img';
-                    $config['allowed_types']        = 'gif|jpg|png|jpeg';
-                    $config['max_size']             = '2048';
+                    $config_img['upload_path']   = './assets/img';
+                    $config_img['allowed_types'] = 'gif|jpg|png|jpeg';
+                    $config_img['max_size']      = '2048';
 
-                    $this->load->library('upload', $config);
+                    $this->upload->initialize($config_img);
 
                     if ($this->upload->do_upload('image')) {
                         $new_image = $this->upload->data('file_name');
@@ -74,11 +90,11 @@ class Test_form extends CI_Controller
                 $portofolio = $_FILES['portofolio']['name'];
 
                 if ($portofolio) {
-                    $config['upload_path']          = './assets/img/portofolio';
-                    $config['allowed_types']        = 'pdf|jpg|png|jpeg';
-                    $config['max_size']             = '2048';
+                    $config_portofolio['upload_path']   = './assets/img/portofolio';
+                    $config_portofolio['allowed_types'] = 'pdf|jpg|png|jpeg';
+                    $config_portofolio['max_size']      = '2048';
 
-                    $this->load->library('upload', $config);
+                    $this->upload->initialize($config_portofolio);
 
                     if ($this->upload->do_upload('portofolio')) {
                         $portofolio = $this->upload->data('file_name');
@@ -90,11 +106,11 @@ class Test_form extends CI_Controller
                 $foto_ktp = $_FILES['foto_ktp']['name'];
 
                 if ($foto_ktp) {
-                    $config_foto_ktp['upload_path']          = './assets/img/foto_ktp';
-                    $config_foto_ktp['allowed_types']        = 'gif|jpg|png|jpeg';
-                    $config_foto_ktp['max_size']             = '2048';
+                    $config_foto_ktp['upload_path']   = './assets/img/foto_ktp';
+                    $config_foto_ktp['allowed_types'] = 'gif|jpg|png|jpeg';
+                    $config_foto_ktp['max_size']      = '2048';
 
-                    $this->load->library('upload', $config_foto_ktp);
+                    $this->upload->initialize($config_foto_ktp);
 
                     if ($this->upload->do_upload('foto_ktp')) {
                         $foto_ktp = $this->upload->data('file_name');
@@ -106,11 +122,11 @@ class Test_form extends CI_Controller
                 $selfie_ktp = $_FILES['selfie_ktp']['name'];
 
                 if ($selfie_ktp) {
-                    $config_selfie_ktp['upload_path']          = './assets/img/selfie_ktp';
-                    $config_selfie_ktp['allowed_types']        = 'gif|jpg|png|jpeg';
-                    $config_selfie_ktp['max_size']             = '2048';
+                    $config_selfie_ktp['upload_path']   = './assets/img/selfie_ktp';
+                    $config_selfie_ktp['allowed_types'] = 'gif|jpg|png|jpeg';
+                    $config_selfie_ktp['max_size']      = '2048';
 
-                    $this->load->library('upload', $config_selfie_ktp);
+                    $this->upload->initialize($config_selfie_ktp);
 
                     if ($this->upload->do_upload('selfie_ktp')) {
                         $selfie_ktp = $this->upload->data('file_name');
@@ -120,19 +136,19 @@ class Test_form extends CI_Controller
                     }
                 }
 
-                $username = $this->input->post('username');
-                $nama_depan = $this->input->post('nama_depan');
-                $nama_belakang = $this->input->post('nama_belakang');
-                $email = $this->input->post('email');
-                $nomor_handphone = $this->input->post('nomor_handphone');
-                $tanggal_lahir = $this->input->post('tanggal_lahir');
-                $tentang_anda = $this->input->post('tentang_anda');
-                $nomor_ktp = $this->input->post('nomor_ktp');
-                $nama_depan_ktp = $this->input->post('nama_depan_ktp');
+                $username          = $this->input->post('username');
+                $nama_depan        = $this->input->post('nama_depan');
+                $nama_belakang     = $this->input->post('nama_belakang');
+                $email             = $this->input->post('email');
+                $nomor_handphone   = $this->input->post('nomor_handphone');
+                $tanggal_lahir     = $this->input->post('tanggal_lahir');
+                $tentang_anda      = $this->input->post('tentang_anda');
+                $nomor_ktp         = $this->input->post('nomor_ktp');
+                $nama_depan_ktp    = $this->input->post('nama_depan_ktp');
                 $nama_belakang_ktp = $this->input->post('nama_belakang_ktp');
-                $jenis_kelamin = $this->input->post('jenis_kelamin');
-                $alamat = $this->input->post('alamat');
-                $kode_pos = $this->input->post('kode_pos');
+                $jenis_kelamin     = $this->input->post('jenis_kelamin');
+                $alamat            = $this->input->post('alamat');
+                $kode_pos          = $this->input->post('kode_pos');
 
                 $this->db->set('username', $username);
                 $this->db->set('nama_depan', $nama_depan);
@@ -149,7 +165,6 @@ class Test_form extends CI_Controller
                 $this->db->where('email', $email);
                 $this->db->update('user');
 
-
                 // $data['data_user'] = $this->db->get_where('user', ['email' => $this->session->userdatrea('email')])->result_array();
                 $data['success'] = $this->db->affected_rows();
                 // $data['title'] = "FREEWORK";
@@ -163,34 +178,11 @@ class Test_form extends CI_Controller
                 // $this->load->view('templates/topbar', $data);
                 $this->load->view('templates/js', $data);
                 $this->load->view('test_form', $data);
-                // $this->load->view('templates/footer');
-            } else {
-                // $data['title'] = "FREEWORK";
-                // $data['kategori'] = $this->model_produk->get_kategori_query();
-                // $data['subkategori'] = $this->model_produk->subkategori();
-                // $data['category'] = $this->model_produk->category();
-                // $data['subcategory'] = $this->model_produk->subcategory();
-                // $data['jenis'] = $this->model_produk->jenis();
-                $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-                // $this->load->view('templates/header', $data);
-                // $this->load->view('templates/topbar', $data);
-                $this->load->view('templates/js', $data);
-                $this->load->view('test_form', $data);
-                // $this->load->view('templates/footer');
-            }
-        } else {
-            // $data['title'] = "FREEWORK";
-            // $data['kategori'] = $this->model_produk->get_kategori_query();
-            // $data['subkategori'] = $this->model_produk->subkategori();
-            // $data['category'] = $this->model_produk->category();
-            // $data['subcategory'] = $this->model_produk->subcategory();
-            // $data['jenis'] = $this->model_produk->jenis();
-            $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-            // $this->load->view('templates/header', $data);
-            // $this->load->view('templates/topbar', $data);
-            $this->load->view('templates/js', $data);
-            $this->load->view('test_form', $data);
             // $this->load->view('templates/footer');
+            } else {
+                return redirect('test_form');
+                // return $this->index();
+            }
         }
     }
 }
